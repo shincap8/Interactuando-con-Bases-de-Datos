9026ce4096792a90779e8f199fcf66747a00f540
shincap8 <43832784+shincap8@users.noreply.github.com>
2018-12-05 16:11:38 -0500

    estoy terminando la verificación de usuario

--- php/server/check_login.php
+++ php/server/check_login.php
@@ -1,9 +1,29 @@
 <?php
     require ('./conector.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password = md5($password);
     $con = new ConectorBD();
     $response['conexion'] = $con->initConexion('agenda_db');
 
-
-
+    //si la conexión fue exitosa
+    if ($response['conexion'] == 'OK') {
+        //Consulta para ver si el usuario ingresado exite en la BD
+        $resultado_consulta = $con->consultar(['usuarios'],['email', 'password'], 'WHERE email="'.$username.'"');
+        if ($resultado_consulta-> num_rows != 0) {
+            $fila = $resultado_consulta->fetch_assoc();
+            if ($password == $fila['password']) {
+                $response['msg'] = 'Redireccionando';
+                $response['acceso'] = 'Usuario Autorizado';
+                $_SESSION['email'] = $fila['email']; //asigno la sesion al usuario actual
+            } else {
+                $response['msg'] = 'Contraseña incorrecta';
+                $response['acceso'] = 'Acceso rechazado';
+            }
+        } else {
+            $response['acceso'] = 'No existen usuarios registrados'; //Mostrar alerta Si no existen usuarios registrados
+            $response['msg'] = 'Presione el botón Inicializar Usuarios'; //Enviar mensajes para registrar los usuarios
+        }
+    }
 
  ?>
